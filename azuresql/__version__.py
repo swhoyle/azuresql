b4539802e6d8ca61436f025c3bbf77f97087c9f1
Spencer Hoyle <150398425+swhoyle@users.noreply.github.com>
2025-03-28 02:07:16 -0700

    Create __version__.py

---
+++ azuresql/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "azuresql"
+__description__ = "A Python Library for managing Azure SQL Servers and Databases."
+__url__ = "https://github.com/swhoyle/azuresql"
+__version__ = "0.0.0"
+__author__ = "Spencer Hoyle"
+__author_email__ = "spencer.w.hoyle@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 Spencer Hoyle"
